8a5a37df043dd38ca32b704d699882e8bf3bc367
Ian Burns <ianb821@gmail.com>
2013-08-28 19:22:48 -0500

    Updated Xcode Sample Project

---
+++ IBActionSheetSample/IBActionSheetSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IBActionSheetSample
+//
+//  Created by Ian Burns on 8/28/13.
+//  Copyright (c) 2013 Ian Burns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IBActionSheetSample/IBActionSheetSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IBActionSheetSample
+//
+//  Created by Ian Burns on 8/28/13.
+//  Copyright (c) 2013 Ian Burns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
